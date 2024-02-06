25c327157ab7c57c89bbf998f5625a28a7ae14cb
Avinash8396 <59692133+Avinash8396@users.noreply.github.com>
2024-02-06 14:37:58 +0530

    Update streamlit_app.py
    Updating the code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import streamlit
 streamlit.title('My Parents New Healty Diner')
 
-
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
